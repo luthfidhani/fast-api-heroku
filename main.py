ab829a3c99e4117d25702d1a8bf5e4e4c9abfa51
Luthfi A. Ardhani <luthfidhani17@gmail.com>
2022-03-08 16:31:32 +0700

    add calc

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from fastapi.params import Form
 app = FastAPI()
 
 @app.get("/")
 def hello():
     return {"message":"Success"}
+
+@app.post("/")
+async def index(query:str = Form(..., example='1+(1+2)*8')):
+    return {"query": query, "result": eval(query)}
